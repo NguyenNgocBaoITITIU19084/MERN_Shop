aae5c8c4f4311ae67962c407ee60bc3d981fdfb3
NguyenNgocBaoITITIU19084 <ititiu19084@student.hcmiu.edu.vn>
2023-10-11 10:25:07 +0700

    sign up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { LoginPage } from "./Routes.js";
+import { LoginPage, SignUpPage } from "./Routes.js";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -2,16 +2,30 @@ import { React, useState } from "react";
 import {AiOutlineEyeInvisible, AiOutlineEye} from 'react-icons/ai'
 import styles from '../../styles/styles'
 import {Link} from 'react-router-dom'
+import {RxAvatar} from 'react-icons/rx';
 
-const Login = () => {
+const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("")
+  const [rePassword,setRePassword] = useState("")
   const [visible, setVisible] = useState(false)
+  const [reVisible, setReVisible] = useState(false)
+  const [avatar,setAvatar] = useState(null)
+
+  const handleSubmit = (e) => {
+    console.log('submit');
+  }
+
+  const handleFileChanged = (e) => {
+    const file = e.target.files['0'];
+    setAvatar(file);
+  }
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className='sm:mx-auto sm:w-full sm:max-w-md'>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                Login to your account
+                Register New Account
             </h2>
         </div>
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
@@ -36,22 +50,46 @@ const Login = () => {
                           }
                       </div>
                     </div>
-                    <div className={`${styles.noramlFlex} justify-between`}>
-                          <div className={`${styles.noramlFlex}`}>
-                          <input type="checkbox" name="remember-me" id="remember-me" className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"/>
-                          <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">  Remember me </label>
-                          </div>
-                          <div className="text-sm">
-                            <a href=".forgot-password" className="font-medium text-blue-600 hover:text-blue-500" > Forgot your password?</a>
-                          </div>
+                    <div>
+                      <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+                      <div className="mt-1 relative">
+                          <input className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm" type={visible ? "text" : "password"}name="password" autoComplete="current-password" required value={rePassword}  onChange={(e) => setRePassword(e.target.value)}></input>
+                          {
+                            reVisible ? (
+                              <AiOutlineEye className="absolute right-2 top-2 cursor-pointer" size={25} onClick={() => setReVisible(false)}/>
+                            ) : (
+                              <AiOutlineEyeInvisible className="absolute right-2 top-2 cursor-pointer" size={25} onClick={() => setReVisible(true)}/>
+                            )
+                          }
+                      </div>
+                    </div>
+                    <div>
+                      <label htmlFor="avatar" className="block text-sm font-medium text-gray-700">
+
+                      </label>
+                      <div className="mt-2 flex items-center">
+                          <span className="inline-block h-8 w-8 rounded-full overflow-hidden">
+                            {
+                              avatar ? (
+                                <img src={URL.createObjectURL(avatar)} alt="avatar" className="h-full w-full object-cover rounded-full"/>
+                              ) : (
+                                <RxAvatar className="h-8 w-8"/>
+                              )
+                            }
+                          </span>
+                          <label htmlFor="file-input" className="ml-5 flex items-enter justify-center px-4 py-2 border border-gray-300 rounded-sm shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
+                            <span>Upload a Avatar</span>
+                            <input type="file" name="avatar" id="file-input" accept=".jpg,.jpeg,.png"  className="sr-only" onChange={handleFileChanged}/>
+                          </label>
+                      </div>
                     </div>
                     <div>
                       <button type="submit" className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700">Submit</button>
                     </div>
                     <div className={`${styles.noramlFlex} w-full`}>
-                      <h4>Not have any account?</h4>
-                      <Link to="/sign-up" className="text-blue-600 pl-2">
-                      Sign Up
+                      <h4>Already have an account</h4>
+                      <Link to="/login" className="text-blue-600 pl-2">
+                      Sign In
                       </Link>
                     </div>
                 </form>
@@ -61,4 +99,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default SignUp
