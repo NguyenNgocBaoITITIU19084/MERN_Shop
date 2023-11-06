68ee3a75b454f1556a78aea2f388f0a42c76ddb2
NguyenNgocBaoITITIU19084 <ititiu19084@student.hcmiu.edu.vn>
2023-11-06 09:02:44 +0700

    featured product

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Layout/Header'
 import Hero from '../components/Route/Hero/Hero'
 import Category from '../components/Route/Category/Category'
 import BestDeals from '../components/Route/BestDeals/BestDeals'
+import FeaturedProducts from '../components/Route/FeaturedProducts/FeaturedProducts'
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
       <Hero />
       <Category />
       <BestDeals />
+      <FeaturedProducts />
     </div>
   )
 }
